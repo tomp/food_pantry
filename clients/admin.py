8561d9e3a44d01142f2d77880c47d609fe0e2284
Tom Pollard <tpollard@flashpoint-intel.com>
2017-08-26 18:05:28 -0400

    Added simple-history to admin

--- clients/admin.py
+++ clients/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from simple_history.admin import SimpleHistoryAdmin
+from .models import Person, Client, Household, Visit
 
-# Register your models here.
+admin.site.register(Person, SimpleHistoryAdmin)
+admin.site.register(Client, SimpleHistoryAdmin)
+admin.site.register(Household, SimpleHistoryAdmin)
+admin.site.register(Visit, SimpleHistoryAdmin)
